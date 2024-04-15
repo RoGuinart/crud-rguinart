e9ae121f2ef8066879e0d554d456286ad6c57ccc
RoGuinart <rguinartvilarnau@gmail.com>
2024-04-15 14:50:00 +0200

    Update to add a tasks/create page

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -12,7 +12,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return view('index');
+        return view('tasks.index');
     }
 
     /**
@@ -28,7 +28,8 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Task::create($request->all());
+        return redirect()->route('tasks.index');
     }
 
     /**
